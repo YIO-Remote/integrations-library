f9ebfffea80ff928c2475f7c1dc96674ee7ab747
Michael Loercher <michaelloercher@web.de>
2020-12-24 08:24:53 +0100

    code style

--- src/yio-model/mediaplayer/tvchannelmodel_mediaplayer.h
+++ src/yio-model/mediaplayer/tvchannelmodel_mediaplayer.h
@@ -96,7 +96,8 @@ class BrowsetvchannelModel : public QObject {
     BrowsetvchannelModel(QObject* parent = nullptr, const QString& id = "", const QString& time = "",
                          const QString& title = "", const QString& subtitle = "", const QString& type = "",
                          const QString& imageUrl = "", const QStringList& commands = {})
-        : m_id(id), m_time(time), m_title(title), m_subtitle(subtitle), m_type(type), m_imageUrl(imageUrl), m_commands(commands) {
+        : m_id(id), m_time(time), m_title(title), m_subtitle(subtitle), m_type(type),
+          m_imageUrl(imageUrl), m_commands(commands) {
         Q_UNUSED(parent)
     }
     ~BrowsetvchannelModel() {}
